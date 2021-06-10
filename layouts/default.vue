2bc99ff973bc8c5a88266b88cda1a3abd74119df
AidanHibbard <aidanchibbard@gmail.com>
2021-06-09 20:43:48 -0700

    Updated NAV

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,46 +1,47 @@
 <template>
-<v-app dark>
-<div id="navbar">
-    <v-app-bar
-      color="deep-purple"
-      dark
-    >
-      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Title</v-toolbar-title>
-    </v-app-bar>
-
-    <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      temporary
-    >
-      <v-list
-        nav
-        dense
+  <v-app dark>
+    <center>
+      <v-app-bar
+        color="deep-purple"
+        dark
+        fixed
       >
-        <v-list-item-group
-          v-model="group"
-          active-class="deep-purple--text text--accent-4"
-        >
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item>
+        <v-toolbar-title>{Aidan Hibbard}</v-toolbar-title>
+        <v-spacer />
+        <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      </v-app-bar>
 
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Account</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-</div>
-</v-app>
+      <v-navigation-drawer
+        v-model="drawer"
+        absolute
+        temporary
+      >
+        <v-list
+          nav
+          dense
+        >
+          <v-list-item-group
+            v-model="group"
+            active-class="deep-purple--text text--accent-4"
+          >
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Home</v-list-item-title>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-account</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Account</v-list-item-title>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
+    </center>
+  </v-app>
 </template>
 
 <script>
@@ -53,7 +54,5 @@ export default {
 </script>
 
 <style scoped>
-#navbar {
-  max-height: 56px;
-}
+
 </style>
