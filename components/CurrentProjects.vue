4e1df92b591a7d563f2e9d8f62fb549db2c97815
Aidan Hibbard <aidanchibbard@gmail.com>
2021-06-24 10:27:54 -0700

    Project Cards and Actions coming along

--- components/CurrentProjects.vue
+++ components/CurrentProjects.vue
@@ -8,8 +8,32 @@
             elevation="6"
         >
             <v-card-title>{{project.name}}</v-card-title>
-            <v-card-subtitle>{{project.updated_at}}</v-card-subtitle>
+            <v-card-subtitle>Latest update: {{project.updated_at.substr(0, 10)}}</v-card-subtitle>
             <v-card-text>{{project.description}}</v-card-text>
+            <v-card-subtitle>Built with: {{project.language}}</v-card-subtitle>
+            <v-card-actions>
+                <v-btn
+                    outlined
+                    rounded
+                    text
+                >
+                    Read More
+                </v-btn>
+                <v-btn
+                    outlined
+                    rounded
+                    text
+                >
+                    Demo
+                </v-btn>
+                <v-btn
+                    outlined
+                    rounded
+                    text
+                >
+                    GitHub
+                </v-btn>
+            </v-card-actions>
         </v-card>
     </v-container>
 </template>
@@ -25,6 +49,24 @@ export default {
 
 <style scoped>
 h1 {
+    margin-top: 0;
     text-align: center;
+    margin-bottom: 0;
+    padding-bottom: 0;
+}
+.v-card {
+    margin: 0 auto;
+    margin-bottom: 20px;
+    max-width: 425px;
+    border-radius: 5px;
+}
+.v-card__title {
+    padding-top: 5px;
+}
+.v-card__subtitle {
+    padding-bottom: 3px;
+}
+.v-card__text {
+    padding-bottom: 5px;
 }
 </style>
