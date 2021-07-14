86fdc3c1a4b865d705b043ac03c748a7c409a2be
AidanHibbard <aidanchibbard@gmail.com>
2021-07-14 13:34:38 -0700

    Getting setup with matter to transform md into json

--- pages/projects/_slug.vue
+++ pages/projects/_slug.vue
@@ -1,27 +1,19 @@
 <template>
     <article>
-        {{page}}
+        <nuxt-content :document="page" />
     </article>
 </template>
 
 <script>
-import Markdown from '@nuxt/content/parsers/markdown'
-import { getDefaults, processMarkdownOptions } from '@nuxt/content/lib/utils'
+import matter from 'gray-matter';
 export default {
     async asyncData({ $axios, params }) {
-        const resp = await $axios.get(`http://192.168.1.101:5000/`)
-        const page = await parseMarkdown(resp.data)
-        console.log(page)
+        const resp = await $axios.get(`http://192.168.1.101:5000/`, { params: { project: params.slug } })
+        const page = await resp.data
+        matter(page)
         return {
             page,
         }
-    },
-    methods : {
-        parseMarkdown: async function(md) {
-            const options = getDefaults()
-            processMarkdownOptions(options)
-            return new Markdown(options.markdown).toJSON(md)
-        },
     },
 }
 </script>
