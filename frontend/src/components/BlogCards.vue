d07650d873f41563300a1b44b31115e1692e248f
AidanHibbard <aidanchibbard@gmail.com>
2022-01-27 19:14:31 -0800

    Tags

--- frontend/src/components/BlogCards.vue
+++ frontend/src/components/BlogCards.vue
@@ -5,17 +5,36 @@
             :key="key"
             class="blog_card"
         >
-            
+            <div 
+                v-if="key % 2 === 0"
+                class="left"
+            >
+            Left
+            </div>
+            <div 
+                v-if="key % 2 !== 0"
+                class="right"
+            >
+            Right
+            </div>
+            <h1>{{ meta.attributes.title }}</h1>
+            <CardTags
+                :tags="meta.attributes.tags"
+            />
         </div>
     </div>
 </template>
 
 <script>
+import CardTags from '@/components/CardTags.vue'
 export default {
     name: 'BlogCards',
     props: {
-        post_metas: Array
-    }
+        post_metas: Array,
+    },
+    components: {
+        CardTags,
+    },
 }
 </script>
 
@@ -26,15 +45,33 @@ export default {
     border: 1px solid black;
     border-radius: 5px;
     margin-bottom: 10px;
+    max-width: 400px;
 }
-@media screen and (max-width: 480px) {
+@media screen and (max-width: 900px) {
     .blog_card {
         width: 75%;
     }
 }
-@media screen and (min-width: 480px) {
+@media screen and (min-width: 900px) {
     .blog_card {
         width: 35%;
     }
+}
+.right {
+    border-radius: 0 5px 5px 0;
+    height: 100%;
+    width: 33%;
+    float: right;
+    background-color: aquamarine;
+}
+.left {
+    border-radius: 5px 0 0 5px;
+    height: 100%;
+    width: 33%;
+    float: left;
+    background-color: lightskyblue;
+}
+h1 {
+    font-size: 15px;
 }
 </style>
---
+++ frontend/src/components/CardTags.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="card_tags">
+        <span
+            v-for="(tag, key) in tags"
+            :key="key"
+            class="tag"
+        >
+            {{ tag }}
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CardTags',
+    props: {
+        tags: Array,
+    },
+}
+</script>
+
+<style scoped>
+.tag {
+    font-size: 12px;
+    padding: 2px;
+    border: 1px solid black;
+    border-radius: 5px;
+}
+</style>
