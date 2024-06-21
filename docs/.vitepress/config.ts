699a7e47a1c77df5a7b840b4f392a334294da2aa
Aidan Hibbard <aidanchibbard@gmail.com>
2024-06-21 10:37:51 -0700

    nuxt content will be a lot better

--- docs/.vitepress/config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vitepress';
-
-// https://vitepress.vuejs.org/config/app-configs
-export default defineConfig({
-  title: 'Aidan Hibard',
-  description: 'My Portfolio',
-  base: '/',
-});
