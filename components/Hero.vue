328845085e4acbaf43bb71d8836376b57d77cfbe
Aidan Hibbard <aidanchibbard@gmail.com>
2024-06-28 19:06:13 -0700

    larger hero text

--- components/Hero.vue
+++ components/Hero.vue
@@ -7,11 +7,11 @@
     <div class="relative isolate overflow-hidden px-6 pt-16 sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
       <div class="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
         <h3 class="text-lg font-semibold text-gray-500">Latest Article:</h3>
-        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Use private NPM modules with Google Cloud Build</h2>
+        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Using private NPM modules with Google Cloud Build</h2>
         <p class="mt-6 text-lg leading-8 text-gray-600">Learn how to secretly mount a .npmrc in your docker image to avoid leaking keys.</p>
         <div class="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
-          <NuxtLink to="/" class="px-3.5 py-2.5 text-lg font-semibold text-white bg-gray-900 shadow-sm rounded hover:shadow-2xl delay-75">Read</NuxtLink>
-          <NuxtLink to="/articles" class="text-sm font-semibold leading-6 text-gray-900">Browse articles <span aria-hidden="true">→</span></NuxtLink>
+          <NuxtLink to="/" class="px-5 py-3 text-lg font-semibold text-white bg-gray-900 shadow-sm rounded hover:shadow-2xl delay-75">Read</NuxtLink>
+          <NuxtLink to="/articles" class="text-md font-semibold leading-6 text-gray-900">Browse articles <span aria-hidden="true">→</span></NuxtLink>
         </div>
       </div>
       <div class="relative sm:mt-8 md:mt-8 lg:mt-16 h-80 lg:mt-8">
