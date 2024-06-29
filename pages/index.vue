9a5d722ad9060465f3bdd56ded56532ded7075e0
Aidan Hibbard <aidanchibbard@gmail.com>
2024-06-28 20:34:36 -0700

    new content section

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@
       <div class="mx-auto max-w-7xl px-6 lg:px-8">
         <div class="mx-auto max-w-2xl lg:mx-0">
           <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Projects I'm working on</h2>
-          <p class="mt-2 text-lg leading-8 text-gray-600">Learn how to grow your business with our expert advice.</p>
+          <p class="mt-2 text-lg leading-8 text-gray-600">Find templates, utilities, and more</p>
         </div>
         <div class="mx-auto mt-4 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-8 border-t border-gray-200 pt-10 sm:mt-8 sm:pt-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           <article class="flex max-w-xl flex-col items-start justify-between">
@@ -21,22 +21,10 @@
               <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
                 <a href="#">
                   <span class="absolute inset-0"></span>
-                  Boost your conversion rate
+                  Olallie
                 </a>
               </h3>
               <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
-            </div>
-            <div class="relative mt-8 flex items-center gap-x-4">
-              <img src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" class="h-10 w-10 rounded-full bg-gray-50">
-              <div class="text-sm leading-6">
-                <p class="font-semibold text-gray-900">
-                  <a href="#">
-                    <span class="absolute inset-0"></span>
-                    Michael Foster
-                  </a>
-                </p>
-                <p class="text-gray-600">Co-Founder / CTO</p>
-              </div>
             </div>
           </article>
           <article class="flex max-w-xl flex-col items-start justify-between">
@@ -48,22 +36,10 @@
               <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
                 <a href="#">
                   <span class="absolute inset-0"></span>
-                  Boost your conversion rate
+                  Elevation Tool
                 </a>
               </h3>
               <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
-            </div>
-            <div class="relative mt-8 flex items-center gap-x-4">
-              <img src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" class="h-10 w-10 rounded-full bg-gray-50">
-              <div class="text-sm leading-6">
-                <p class="font-semibold text-gray-900">
-                  <a href="#">
-                    <span class="absolute inset-0"></span>
-                    Michael Foster
-                  </a>
-                </p>
-                <p class="text-gray-600">Co-Founder / CTO</p>
-              </div>
             </div>
           </article>
           <article class="flex max-w-xl flex-col items-start justify-between">
@@ -75,26 +51,12 @@
               <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
                 <a href="#">
                   <span class="absolute inset-0"></span>
-                  Boost your conversion rate
+                  Skatehouse Media
                 </a>
               </h3>
               <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
             </div>
-            <div class="relative mt-8 flex items-center gap-x-4">
-              <img src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" class="h-10 w-10 rounded-full bg-gray-50">
-              <div class="text-sm leading-6">
-                <p class="font-semibold text-gray-900">
-                  <a href="#">
-                    <span class="absolute inset-0"></span>
-                    Michael Foster
-                  </a>
-                </p>
-                <p class="text-gray-600">Co-Founder / CTO</p>
-              </div>
-            </div>
           </article>
-
-          <!-- More posts... -->
         </div>
       </div>
     </div>
