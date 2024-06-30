0f69d8ee0ffae9764450de311e017c35f67a181f
Aidan Hibbard <aidanchibbard@gmail.com>
2024-06-30 12:30:50 -0700

    more spacing changes, footer fixes

--- components/Hero.vue
+++ components/Hero.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="sm:mt-0 md:mt-8 lg:mt-8">
+  <div class="sm:mt-0 md:mt-0 lg:mt-14">
     <div class="mx-auto max-w-7xl py-18 sm:px-6 sm:py-1">
       <div class="relative isolate overflow-hidden px-6 pt-8 sm:px-16 md:pt-10 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
         <div class="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-8 lg:text-left">
