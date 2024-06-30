5b836b3a1a89f38318bc6216b2efe6b52170138b
Aidan Hibbard <aidanchibbard@gmail.com>
2024-06-29 22:15:18 -0700

    fix spacing in hero

--- pages/index.vue
+++ pages/index.vue
@@ -60,7 +60,7 @@
         </div>
       </div>
     </div>
-    <div class="relative isolate overflow-hidden bg-white px-6 py-12 sm:py-16 lg:overflow-visible lg:px-0">
+    <div class="relative isolate overflow-hidden bg-white px-6 py-12 sm:py-8 lg:overflow-visible lg:px-0">
       <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
         <div class="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
           <div class="lg:pr-4">
