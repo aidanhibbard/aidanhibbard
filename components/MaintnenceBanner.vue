7c150f007cf9aab57c7e9d1ae6b125f3f505bd61
Aidan Hibbard <aidanchibbard@gmail.com>
2024-07-20 20:38:23 -0700

    Update MaintnenceBanner.vue

--- components/MaintnenceBanner.vue
+++ components/MaintnenceBanner.vue
@@ -1,9 +1,5 @@
 <template>
-  <div class="flex justify-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
-    <div class="flex flex-wrap justify-center gap-x-4 gap-y-2">
-      <p class="text-md leading-6 text-gray-900 center-text">
-        This site is currently being rebuilt with TailwindUI
-      </p>
-    </div>
-  </div>
+  <p class="text-lg leading-6 text-gray-900 text-center py-2 bg-gradient-to-r from-blue-200 to-orange-100">
+    This site is currently being rebuilt with TailwindUI
+  </p>
 </template>
