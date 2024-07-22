153d6178859e113733267d6d1516683053aeada3
Aidan Hibbard <aidanchibbard@gmail.com>
2024-07-21 21:30:07 -0700

    add nuxt robots plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,8 @@ export default defineNuxtConfig({
     '@nuxt/image',
     '@nuxt/eslint',
     '@nuxtjs/color-mode',
-    '@nuxt/ui'
+    '@nuxt/ui',
+    '@nuxtjs/robots'
   ],
   css: ['~/assets/css/tailwind.css'],
   postcss: {
