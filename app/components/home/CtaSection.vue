547aaa051b12b5ab5ec1bd561db2c8a6ff109bb3
Aidan Hibbard <aidanchibbard@gmail.com>
2025-02-25 14:17:30 -0800

    update cta with latest article

--- app/components/home/CtaSection.vue
+++ app/components/home/CtaSection.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
-
+const { data: article } = await useAsyncData(
+  'article',
+  async () => queryCollection('articles')
+    .order('publishedAt', 'DESC')
+    .select('title', 'desc')
+    .first(),
+)
 </script>
 
 <template>
@@ -8,10 +14,10 @@
       <div class="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
         <div class="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
           <h2 class="text-3xl font-semibold tracking-tight text-balance text-white sm:text-4xl">
-            Boost your productivity. Start using our app today.
+            {{ article?.title }}
           </h2>
           <p class="mt-6 text-lg/8 text-pretty text-gray-300">
-            Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla.
+            {{ article?.desc }}
           </p>
           <div class="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
             <a
@@ -24,7 +30,6 @@
             >Browse articles <span aria-hidden="true">→</span></a>
           </div>
         </div>
-        <div class="relative mt-16 h-80 lg:mt-8" />
       </div>
     </div>
   </div>
