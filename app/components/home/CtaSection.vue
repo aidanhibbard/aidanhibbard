a43a4209330a72b56c0747f66d819ebb729f2f75
Aidan Hibbard <aidanchibbard@gmail.com>
2025-02-26 07:26:51 -0800

    Update CtaSection.vue

--- app/components/home/CtaSection.vue
+++ app/components/home/CtaSection.vue
@@ -21,7 +21,7 @@ const { data: article } = await useAsyncData(
           </p>
           <div class="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
             <NuxtLink
-              :to="article?.stem"
+              :to="`/${article?.stem}`"
               class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-xs hover:bg-gray-100 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
             >Read more</NuxtLink>
             <NuxtLink
