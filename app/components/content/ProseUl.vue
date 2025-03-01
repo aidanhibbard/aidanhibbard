61524aa738742fafa1c385bf4c72d74050f90734
Aidan Hibbard <aidanchibbard@gmail.com>
2025-02-28 20:14:40 -0800

    lint

--- app/components/content/ProseUl.vue
+++ app/components/content/ProseUl.vue
@@ -1,5 +1,5 @@
 <template>
-  <ul class="ml-4 list-disc prose lg:prose-lg py-2 text-gray-800 leading-relaxed transition-all duration-300">
+  <ul class="pl-4 list-disc prose lg:prose-lg py-2 text-gray-800 leading-relaxed transition-all duration-300">
     <slot mdc-unwrap="ul" />
   </ul>
 </template>
