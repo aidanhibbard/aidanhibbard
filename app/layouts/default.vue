e5c85a3bd0b24f398ff8990a5febbae973a0add8
Aidan Hibbard <aidanchibbard@gmail.com>
2025-03-06 21:30:24 -0800

    hide mobile bar

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-full bg-white dark:bg-[#1c1f24]">
     <NavigationDesktopBar class="hidden md:block" />
-    <NavigationMobileBar />
+    <NavigationMobileBar class="md:hidden sm:block" />
     <main class="relative h-full">
       <slot />
     </main>
