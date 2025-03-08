8c042e888e778ad9b08ed320cd30255345ed5ae3
Aidan Hibbard <aidanchibbard@gmail.com>
2025-03-07 18:38:43 -0800

    change up verbiage

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,12 +4,12 @@
     <SharedLatestContent
       type="articles"
       title="Latest Articles"
-      description="Read about tricks, tips, and more."
+      description="Read about tips, tricks, and the latest in the web."
     />
     <SharedLatestContent
       type="projects"
       title="What I'm working on"
-      description="Find templates, utils, and more."
+      description="Find utils, templates, components, and more."
     />
   </div>
 </template>
