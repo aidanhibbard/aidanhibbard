92b3bfdc478969657f0e882aa698e8c91221e4a2
Aidan Hibbard <aidanchibbard@gmail.com>
2025-03-15 14:14:04 -0700

    remove hash on links

--- app/components/content/ProseH2.vue
+++ app/components/content/ProseH2.vue
@@ -13,7 +13,7 @@ const generate = computed(() => props.id && ((typeof headings?.anchorLinks === '
       :href="`#${props.id}`"
       class="text-2xl text-gray-900 dark:text-[#F3F4ED] hover:text-teal-600 font-bold no-underline transition-all duration-300 inline-flex items-center"
     >
-      # <slot md-unwrap="h2" />
+      <slot md-unwrap="h2" />
     </NuxtLink>
     <slot v-else />
   </h2>
