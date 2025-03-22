db2f30b2ddf7bc039f2aca588b8ea4286e1a5d78
Aidan Hibbard <aidanchibbard@gmail.com>
2025-03-22 13:34:06 -0700

    update readme

--- app/components/shared/ContentHeader.vue
+++ app/components/shared/ContentHeader.vue
@@ -9,12 +9,6 @@ defineProps<{
 
 <template>
   <section class="py-4">
-    <NuxtImg
-      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoqMQm4KuOCUn2pV2AOHEBPUxLMXzrLN7eCg&s"
-      :alt="content.title"
-      class="w-full h-64 object-cover rounded-lg mb-4"
-    />
-
     <h1 class="text-3xl font-bold text-gray-900 dark:text-gray-100 mb-2">
       {{ content.title }}
     </h1>
@@ -32,8 +26,8 @@ defineProps<{
         v-for="tag in content.tags"
         :key="tag"
         class="hover:cursor-pointer px-3 py-1 text-sm font-medium rounded-full transition-colors duration-200
-           bg-gray-200 text-gray-800 hover:bg-teal-500 hover:text-white
-           dark:bg-gray-700 dark:text-gray-200 dark:hover:bg-teal-500 dark:hover:text-white"
+            bg-gray-200 text-gray-800 hover:bg-teal-500 hover:text-white
+            dark:bg-gray-700 dark:text-gray-200 dark:hover:bg-teal-500 dark:hover:text-white"
       >
         {{ tag }}
       </span>
---
+++ content/articles/ai-coding-delayed-the-mvp.md
@@ -0,0 +1,10 @@
+---
+title: 'AI Delayed the MVP'
+desc: 'Untested, undocumented, code is becoming concerningly prevelant.'
+publishedAt: 06-17-24
+lastEditedAt: 06-17-24
+path: '/articles/install-private-npm-modules-in-cizx'
+tags:
+  - 'AI'
+  - 'Cursor'
+---
