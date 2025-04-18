64e4eb17ecfdabcad364c8a320cb82b036b04a84
Aidan Hibbard <aidanchibbard@gmail.com>
2025-04-17 18:51:32 -0700

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,30 +1,13 @@
-<script setup lang="ts">
-definePageMeta({
-  layout: false,
-})
+<script setup lang='ts'>
+
 </script>
 
 <template>
-  <main class="h-full p-4 grid grid-cols-1 md:grid-cols-4 gap-2 lg:grid-cols-6">
-    <!-- 1 Large box spanning 2x2 -->
-    <div class="rounded-4xl md:col-span-2 md:row-span-1 bg-white" />
-    <!-- 2 Tall box spanning 2 rows -->
-    <div class="rounded-4xl md:row-span-1 bg-white" />
-    <!-- 3 Regular box -->
-    <div class="rounded-4xl md:row-span-3 lg:row-span-1 bg-white" />
-    <!-- 4 Wide box spanning 2 columns -->
-    <div class="rounded-4xl md:col-span-2 bg-white" />
-    <!-- 5 Tall box spanning 2 rows -->
-    <div class="rounded-4xl md:row-span-2 lg:col-span-2 bg-white" />
-    <!-- 6 Regular boxes -->
-    <div class="rounded-4xl md:col-span-2 bg-white" />
-    <!-- 7  box -->
-    <div class="rounded-4xl lg:col-span-2 bg-white" />
-    <!-- 8 Wide box spanning 3 columns -->
-    <div class="rounded-4xl md:col-span-3 bg-white" />
-    <!-- 9 Regular boxes -->
-    <div class="rounded-4xl lg:col-span-2 bg-white" />
-    <!-- 10 box -->
-    <div class="rounded-4xl col-span-full lg:col-span-1 bg-white" />
-  </main>
+    <div>
+
+    </div>
 </template>
+
+<style>
+
+</style>
