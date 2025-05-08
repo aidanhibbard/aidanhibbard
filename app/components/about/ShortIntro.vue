25151acaa5c0e14d3d0cd4829f1b13ef65a15e09
Aidan Hibbard <aidanchibbard@gmail.com>
2025-05-08 15:49:16 -0700

    update about text

--- app/components/about/ShortIntro.vue
+++ app/components/about/ShortIntro.vue
@@ -2,7 +2,7 @@
   <div class="prose prose-lg mx-auto mb-16 text-gray-900 dark:text-[#eeeeee]">
     <h1 class="text-5xl font-extrabold text-gray-900 dark:text-[#eeeeee]">About</h1>
     <p>
-      Experienced in building sleek, and resilient services, I've worked with analysts and designers to create business solutions that meet user needs.
+      Experienced in building sleek, and resilient services, I've worked with analysts and designers to create solutions that meet user needs.
     </p>
     <p>
       I'm passionate about working across the stack, from building ETL pipelines with data driven dashboards, to maintaining internal modules and libraries.
