0870d20029ecec5433bfb09aa83ad78167d0a69b
Aidan Hibbard <aidanchibbard@gmail.com>
2025-11-04 16:40:05 -0800

    fix bg

--- app/components/landing/animations/HeroBackground.vue
+++ app/components/landing/animations/HeroBackground.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="fixed inset-0 -z-10 overflow-hidden">
+  <div
+    class="absolute inset-0 -z-10 overflow-hidden pointer-events-none"
+    aria-hidden="true"
+  >
     <div class="absolute inset-0 grid-background">
       <svg
         class="absolute inset-0 h-full w-full"
