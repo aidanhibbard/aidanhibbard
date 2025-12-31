7a9508ddb6e9fef92f99d3c8b60fdfb32a10e445
Aidan Hibbard <aidanchibbard@gmail.com>
2025-12-30 16:33:22 -0800

    about page and server dir

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -5,8 +5,30 @@
       Born, and raised in the beautiful mountain town of Bend, Oregon I've been incredibly fortunate to live in a tech focused community that is passionate about the outdoors.
     </ProseP>
     <ProseP>
+      I picked up web development back in the twenty-teens with Adobe Dreamweaver, and Java backends. It felt so empowering to be able to build services that anyone could access.
+      Picking up JQuery, it was so fun to build interactive sites, didn't have to be anything impressive, but buttons that made things spin, or playing with tags like marquee.
+      Web back then was already a growing, and leading technology so I knew it was something I should stick with.
+      Flash forward to today, and now we can build things like games, full native apps, and more.
+    </ProseP>
+    <ProseP>
+      Once Node backends became more popular, and TypeScript was picking up in adoption it was a very smooth transition over.
+      JavaScript syntax, with the strict types that you can get from Java? Count me in.
+    </ProseP>
+    <ProseP>
       I started my software career at a company called G5 in Bend (later purchased by RealPage) as a QA engineer writing E2E tests using Selenium, Ruby, and Rails.
       It was in that role where I gained a deep respect for best practices, UX, accessability, and those are values I carry in my development still.
+    </ProseP>
+    <ProseP>
+      Eventually I started picking up some developer tickets to get a taste of what professional development was like.
+      I never wanted to stop.
+      Swiftly they moved me onto a team of three developers including myself, and I spent years learning from incredible senior developers.
+      We managed a "fleet" of Rails apps all setup to run as microservices in API mode, and used gRPC, as well as google pubsub to handle cross communications.
+      This was one of the most fun stacks I had ever worked on, you'd have nine terminal sessions open watching logs between services to find an issue in the transformation pipeline.
+      I had the incredible oppurtunity to work with some DevOps engineers who hosted our apps in Kubernetes, managed our CI, and helped review our SQL in GCP for long running queries.
+      Getting hands on expierence with Kubernetes at enterprise scale, learning how to setup IAC, and setting up CI in complex systems are skills I still value to these days.
+    </ProseP>
+    <ProseP>
+      Since then I have had an absolute pleasure working on unique, and intricate projects as well as meting some of the best developers.
     </ProseP>
   </div>
 </template>
